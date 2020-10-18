6ac8c989e1ef278445650f40796356e6312b58a9
Halyna <mykhalchukhalyna@gmail.com>
2020-10-18 19:57:16 +0300

    Added tasks 5-6, began task 9

---
+++ script5.js
@@ -0,0 +1,23 @@
+// let str = prompt('Enter a string, you want to check')
+// n = 0;
+// console.log('You entered: ', str);
+
+// function bracketsCheck(str) {
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === '(') {
+//             n++
+//         }
+//         if (str[i] === ')') {
+//             n--
+//         }
+//     }
+
+//     if (n == 0) {
+//         console.log('Correct');
+//     } else {
+//         console.log('Incorrect');
+//     }
+// }
+
+
+// bracketsCheck(str)
---
+++ script6.js
@@ -0,0 +1,8 @@
+// let exp = prompt('Enter an expression to calculate')
+// console.log('You entered: ', exp);
+// try {
+//     let res = eval(exp);
+//     console.log('Result: ', res);
+// } catch (err) {
+//     console.log('Incorrect expression');
+// }
